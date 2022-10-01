8d894be71e55de7bf964af7e02fd1884f5aeb23f
Antony Ekirapa <85803523+NueMe@users.noreply.github.com>
2022-10-01 05:07:22 +0300

    Update routes

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 app.use("/", require("./routes/index"));
-app.use("/dashboard", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 8600;
 
